ffd16254504c7d72f268e06eadb59f9b62586394
jhackett1 <joshua.hackett93@gmail.com>
2018-04-22 18:05:46 +0100

    Added full styling

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,6 @@ router.get('/', function(req, res, next) {
 
 // POST form
 router.post('/', function(req, res, next){
-
   // Calulate totals
   let subTotal = 0;
   req.body.items.forEach(function(item){
@@ -22,14 +21,18 @@ router.post('/', function(req, res, next){
   })
   let grandTotal = subTotal - req.body.deductions;
 
-  // Create a HTML document from data and ejs template
-  ejs.renderFile(path.join(__dirname, '../views/template.ejs'), {
+  let data = {
     invoiceData: req.body,
     agencyData: agencyData,
-    dateIssued: new Date(),
+    dateIssued: new Date().toDateString(),
     subTotal: subTotal,
     grandTotal: grandTotal
-  })
+  }
+
+  data.invoiceData.invoiceTo = data.invoiceData.invoiceTo.replace(new RegExp('\n', 'g'), '<br/>')
+
+  // Create a HTML document from data and ejs template
+  ejs.renderFile(path.join(__dirname, '../views/template.ejs'), data)
     .then(html=>{
       saveHTML(html);
     })
@@ -48,6 +51,13 @@ router.post('/', function(req, res, next){
     const htmlToPDF = new HTMLToPDF({
       inputPath: path.join(__dirname, '../public/output.html'),
       outputPath: path.join(__dirname, '../public/output.pdf'),
+      templatePath: path.join(__dirname, "../template"),
+      include: [
+        {
+          type: 'css',
+          filePath: path.join(__dirname, '../public/invoice.css'),
+        }
+      ],
     })
     htmlToPDF.build((error) => {
       if(error) throw error;
